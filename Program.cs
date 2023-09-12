95100cfc7bf7f84c16884f722edaf686ccc95cd5
aluno <Aluno@PR01-2D-36.CSED.LAB>
2023-09-12 20:54:48 -0300

    o jonathan é burro pra krl

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Program.cs{
+
+class Clientes{
+    int id;
+
+    string nome;
+
+    string cpf;
+
+    string email;
+
+    string hash;
+
+    public string getNome(){
+        return nome;
+    }
+    public string getCpf(){
+        return cpf;
+    }
+    public string getEmail(){
+        return email;
+    }
+    public string getHash(){
+        return hash;
+    }
+
+    public void setNome(string n)
+      
+}
+
+class BaseClientes{
+
+}
+
+
+
+}
+
